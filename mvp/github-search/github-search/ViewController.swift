4c768f938f03b2f632b793b45ecd360a26021206
Kazumasa Wakamori <wakamori.kazumasa.17@shizuoka.ac.jp>
2021-11-04 08:14:54 +0900

    Add new mvp app

---
+++ mvp/github-search/github-search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  github-search
+//
+//  Created by 若森和昌 on 2021/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
